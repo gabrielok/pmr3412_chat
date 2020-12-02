f4705746fc691a7b8150c56b0a304d745e32bb36
Gabriel <gabrielokamoto@hotmail.com>
2020-12-02 18:29:52 -0300

    Added the server file

---
+++ server.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  2 16:03:53 2020
+
+@author: Gabriel
+"""
+
+import websockets
+import asyncio
+import datetime
+
+async def time(websocket, path):
+    while True:
+        now = datetime.datetime.utcnow().isoformat() + "Z"
+        await websocket.send(now)
+        await asyncio.sleep(3)
+
+start_server = websockets.serve(time, "127.0.0.1", 5678)
+print('Connected!')
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
--- src/App.svelte
+++ src/App.svelte
@@ -1,26 +1,34 @@
 <script>
-	export let name;
-
-	let count = 0;
-	function handleClick() {
-		count += 1;
+	var ws = new WebSocket("ws://127.0.0.1:5678/"),
+		messages = document.createElement('u1');
+	// ws.onmessage = function (event) {
+	// 	var messages = document.getElementsByTagName('ul')[0],
+    //         message = document.createElement('li'),
+    //         content = document.createTextNode(event.data);
+    //     message.appendChild(content);
+    //     messages.appendChild(message);
+    // };
+	ws.onopen = function (event) {
+		showmessage = true;
+		console.log('Connected')
 	}
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<button on:click={handleClick}>
-		Clicked {count} {count === 1 ? 'time' : 'times'}
-	</button>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Hello!</h1>
 </main>
 
 <style>
+	body {
+		background-color: #333942;
+	}
 	main {
 		text-align: center;
-		padding: 1em;
+		padding: 5px;
 		max-width: 240px;
 		margin: 0 auto;
+		background-color: #333942;
+		height: 100%;
 	}
 
 	h1 {
@@ -28,6 +36,10 @@
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
+	}
+
+	p {
+		text-align: center;
 	}
 
 	@media (min-width: 640px) {
