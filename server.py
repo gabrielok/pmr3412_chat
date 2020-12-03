eb723d949e91e6c8ea20125641fdda6da9418dcd
Gabriel <gabrielokamoto@hotmail.com>
2020-12-03 18:03:39 -0300

    Completely functional

--- server.py
+++ server.py
@@ -7,17 +7,118 @@ Created on Wed Dec  2 16:03:53 2020
 
 import websockets
 import asyncio
-import datetime
 
-async def time(websocket, path):
-    while True:
-        now = datetime.datetime.utcnow().isoformat() + "Z"
-        await websocket.send('Robson!Oi')
-        await websocket.send('Manuel!oi')
-        await asyncio.sleep(5)
+connected = {} # dictionary of websockets and their usernames
+exitApp = False
+sys = {'welcome': 'Bem vindo. Identifique-se com /nome seunome',
+'newname': 'Nome alterado para ',
+'newuser': ' entrou na sala.',
+'userleft': ' saiu da sala.',
+'namechange': ' mudou o nome para ',
+'nametaken': 'Esse nome já foi escolhido. Tente novamente.'}
 
-start_server = websockets.serve(time, "127.0.0.1", 5678)
-print('Connected!')
+def parse(message):
+    global sys
+    msg_start = message.find('!')
+    command_name = message.find('/nome')
+    command_pm = message.find('/pm')
+    if (command_name >= 0): # change username
+        old = message[0:msg_start]
+        new = message[msg_start + 7:]
+        if (old == ''): # didnt have a name
+            msg_content1 = ''
+            msg_content2 = new
+            msg_all = 0
+        else: # already had a name
+            msg_content1 = old
+            msg_content2 = new
+            msg_all = -1
+        msg_sender = 'system'
+        msg_type = '#'
+    elif (command_pm >= 0): # private message
+        # determine the receiver of the pm
+        rstart = command_pm + 4
+        rend = message[rstart + 1:].find(' ') + rstart + 1
+        receiver = message[rstart:rend]
+        msg_content1 = receiver + '~'
+        msg_content2 = message[rend + 1:]
+        msg_sender = message[0:msg_start] + ' (privado)'
+        msg_type = '~'
+        msg_all = 1
+    else: # regular broadcast message
+        msg_content1 = ''
+        msg_content2 = message[msg_start + 1:]
+        msg_sender = message[0:msg_start]
+        msg_type = '!'
+        msg_all = 1
+    out = [msg_sender, msg_type, msg_content1, msg_content2, msg_all]
+    return out
 
+async def register(websocket):
+    connected[websocket] = ''
+    welcome_message = 'announcement#' + sys['welcome']
+    await websocket.send(welcome_message)
+    print('New user connected')
+
+async def unregister(websocket):
+    for conn in connected:
+        if conn != websocket:
+            msg_send = 'announcement#' + connected[websocket] + sys['userleft']
+            await conn.send(msg_send)
+    print(connected[websocket] + ' disconnected')
+    connected.pop(websocket)
+
+async def main(websocket, path):
+    # this function is called whenever a new connection is established
+    await register(websocket)
+    try:
+        # this for loop ends when the connection closes
+        async for message in websocket:
+            # parse the received messages
+            msg = parse(message)
+
+            # determine whether the sender wants to change name
+            if msg[4] == -1:
+                taken = False
+                for conn in connected:
+                    if msg[3] == connected[conn]:
+                        taken = True
+            elif msg[4] == 0:
+                taken = False
+                for conn in connected:
+                    if msg[3] == connected[conn]:
+                        taken = True
+
+            # decide who to forward the message to
+            for conn in connected:
+                if conn == websocket:
+                    if msg[4] != 1:
+                        # sender tried to change name
+                        if taken:
+                            # sender failed name change
+                            msg_send = msg[0] + msg[1] + sys['nametaken']
+                        else:
+                            # first time or not, same message
+                            msg_send = msg[0] + msg[1] + sys['newname'] + msg[3]
+                            msg_name = 'namechanged' + msg[1] + msg[3]
+                            await conn.send(msg_name)
+                            connected[websocket] = msg[3] # update the name on the dictionary
+                        await conn.send(msg_send)
+                else:
+                    if msg[4] == 1:
+                        # same message for everyone but the sender
+                        msg_send = msg[0] + msg[1] + msg[2] + msg[3]
+                        await conn.send(msg_send)
+                    elif taken == False:
+                        if msg[4] == -1: # not the first time
+                            msg_send = msg[0] + msg[1] + msg[2] + sys['namechange'] + msg[3]
+                        elif msg[4] == 0: # first time
+                            msg_send = msg[0] + msg[1] + msg[3] + sys['newuser']
+                        await conn.send(msg_send)
+    finally:
+        await unregister(websocket)
+
+start_server = websockets.serve(main, "127.0.0.1", 5678)
 asyncio.get_event_loop().run_until_complete(start_server)
+print('Started! Waiting for connections')
 asyncio.get_event_loop().run_forever()
--- src/App.svelte
+++ src/App.svelte
@@ -1,42 +1,91 @@
 <script>
-	let msg_toSend = '', msg_string = '', msg = [];
+	let user_input = '', msg_sent = '', msg_history = [], msg_string = '';
 	let msg_received = {}, msg_content = [];
-	let username = 'Gabriel';
+	var username = '';
 	// {sender: 'Robson', text: 'adsd'}
 	let connected = false;
-
+	var ws;
 	function connect(event) {
 		if (connected == false) {
-			let ws = new WebSocket("ws://127.0.0.1:5678/");
+			ws = new WebSocket("ws://127.0.0.1:5678/");
 			ws.onopen = function(event) {
-				console.log('Connected')
+				console.log('Connected');
 				connected = true;
 			}
 			ws.onmessage = function(event) {
+				console.log(event.data);
 				if (event.data.indexOf('!') >= 0) { // regular message
-					msg_content = event.data.split('!') //! is used as a separator
-					msg_received = {sender: msg_content[0], text: msg_content[1]}
-					msg.push(msg_received);
-					console.log(msg);
+					// format: Sender~Text
+					msg_content = event.data.split('!'); //! is used as a separator
+					msg_received = {sender: msg_content[0], text: msg_content[1]};
+					msg_history.push(msg_received);
+					msg_history = msg_history;
 				} else if (event.data.indexOf('#') >= 0) { // system message
-					msg_content = '';
+					// format: type~Text
+					msg_content = event.data.split('#');
+					if (msg_content[0] == 'namechanged') {
+						username = msg_content[1];
+					} else {
+						msg_received = {sender: 'system', text: msg_content[1]};
+						msg_history.push(msg_received);
+						msg_history = msg_history;
+					}
+				} else if (event.data.indexOf('~') >= 0) { // private message
+					// format: Sender~Receiver~Text
+					msg_content = event.data.split('~');
+					if (msg_content[1] == username) {
+						msg_received = {sender: msg_content[0], text: msg_content[2]};
+						msg_history.push(msg_received);
+						msg_history = msg_history;
+					}
 				}
 		    }
 			ws.onclose = function(event) {
+				console.log('Disconnected');
+				alert('Desconectado')
 				connected = false;
+				username = '';
+				msg_history = [];
+			}
+			ws.onerror = function(event) {
+				console.log('Failed to connect');
 			}
 		}
 	}
 
 	function sendMessage(event) {
 		if (event.keyCode == 13 || event.type == 'click') {
-			if (msg_toSend != '' & 1) {
-				// msg.push(msg_toSend);
-				// msg_type.push(1);
-				// msg_string = msg.join('<br/>');
-
-				console.log(msg_toSend);
-				msg_toSend = '';
+			if (user_input == '') {
+				alert('Digite algo!');
+			} else {
+				let command_name = user_input.indexOf('/nome');
+				let command_pm = user_input.indexOf('/pm');
+				if (username == '') { // does not have a name
+					if (command_name >= 0) { // is trying to set up a name
+						msg_string = '!' + user_input;
+						ws.send(msg_string);
+						console.log('Sent:' + msg_string);
+						user_input = '';
+					} else {
+						alert('Escolha um nome de usuário primeiro.')
+						user_input = '';
+					}
+				} else { // already has a name
+					if (command_name >= 0) { // is trying change name
+						msg_string = username + '!' + user_input;
+						ws.send(msg_string);
+						console.log('Sent:' + msg_string);
+						user_input = '';
+					} else {
+						msg_sent = {sender: username, text: user_input};
+						msg_history.push(msg_sent);
+						msg_history = msg_history;
+						msg_string = username + '!' + user_input;
+						ws.send(msg_string);
+						console.log('Sent:' + msg_string);
+						user_input = '';
+					}
+				}
 			}
 		}
 	}
@@ -44,7 +93,7 @@
 
 <main>
 	<h1>Chat room</h1>
-	<div id='chatbox'>
+	<div id='container'>
 		{#if connected == false}
 			<p id='wait'>
 				Waiting...
@@ -57,16 +106,23 @@
 				Connected!
 			</p>
 		{/if}
-		<p>{@html msg_string}</p>
-		{#each msg as m}
-			{#if 1>0}
-				<p>1</p>
-			{/if}
-		{/each}
-		<input id='input' name='msg' bind:value={msg_toSend} on:keypress={sendMessage}/>
+		<input id='input' bind:value={user_input} on:keypress={sendMessage}/>
 		<button id='send' on:click = {sendMessage}>
 			Enviar
 		</button>
+		<div id='chatbox'>
+			<div>
+				{#each msg_history as m}
+					{#if m.sender == 'system'}
+						<p id='system'>Sistema: {m.text}</p>
+					{:else if m.sender == username}
+						<p id='sent'>Enviado: {m.text}</p>
+					{:else}
+						<p id='received'>{m.sender}: {m.text}</p>
+					{/if}
+				{/each}
+			</div>
+		</div>
 	</div>
 </main>
 
@@ -98,8 +154,8 @@
 		}
 	}
 
-	#chatbox {
-		background-color: #d6d6d6;
+	#container {
+		background-color: #e0e0e0;
 		border-style:double;
 		border-color: #333333;
 		border-width: 9px;
@@ -109,8 +165,20 @@
 		max-width: 600px;
 		min-width: 40%;
 		height: 400px;
-		overflow: auto;
+		overflow: hidden;
 		position: relative;
+	}
+
+	#chatbox {
+		position: absolute;
+		width: 92%;
+		left: 4%;
+		height: 62%;
+		border-style: solid;
+		border-width: 3px;
+		overflow: auto;
+		display: flex;
+		flex-direction: column-reverse;
 	}
 
 	#send {
@@ -149,5 +217,26 @@
 	#ready {
 		color: #34e80c;
 		font-weight: 500;
+	}
+
+	#received {
+		color: #2d1ad6;
+		margin-top: 0.3em;
+		margin-bottom: 0;
+		margin-left: 0;
+	}
+
+	#sent {
+		color: #000000;
+		margin-top: 0.3em;
+		margin-bottom: 0;
+		margin-left: 0;
+	}
+
+	#system {
+		color: #ffa500;
+		margin-top: 0.3em;
+		margin-bottom: 0;
+		margin-left: 0;
 	}
 </style>
